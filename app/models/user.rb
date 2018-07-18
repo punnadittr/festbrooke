bbd2d974d8368e0836a3dbfefed3472422ad57d8
Punnadit Trussat <punnadittr@gmail.com>
2018-07-18 15:13:35 +0700

    add friend request menu, improved form styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  has_many :received_friend_requests, 
+            :foreign_key => 'recipient_id', 
+            :class_name => 'FriendRequest', 
+            :dependent => :destroy
+            
+  has_many :sent_friend_requests, 
+            :foreign_key => 'sender_id', 
+            :class_name => 'FriendRequest', 
+            dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :omniauthable, :validatable
-
-  has_many :received_friend_requests, :foreign_key => 'recipient_id', :class_name => 'FriendRequest'
-  has_many :sent_friend_requests, :foreign_key => 'sender_id', :class_name => 'FriendRequest'
 end
