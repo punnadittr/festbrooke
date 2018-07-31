075a7dc97caaa84bb16bbcea0598fde91921528c
Punnadit Trussat <punnadittr@gmail.com>
2018-07-31 09:00:19 +0700

    Add post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user1)
+  end
+
+  test 'should allow text only post' do
+    post = @user.posts.build(content: 'hello')
+    assert post.valid?
+  end
+
+  test 'should associate with likes' do
+    post = @user.posts.build(content: 'hello')
+    assert_not post.likes.nil?
+  end
 end
